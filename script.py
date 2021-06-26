8b4f80a9b1bfe4769034c1ff43858361c9cebb94
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-06-26 11:53:44 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,15 +1,9 @@
-
 from selenium import webdriver
-from PIL import Image
-import time
 
 driver = webdriver.Chrome()
-driver.get('https://t.me/s/durov')
-
+driver.get('https://t.me/s/dvachannel')
 
 driver.set_window_size(10000, 10000)
-
-
 
 for i in range(1, 100):
     try:
@@ -18,16 +12,5 @@ for i in range(1, 100):
         i = i-1
         ele = driver.find_element_by_xpath(f'/html/body/main/div/section/div[{i}]' + '/div[1]/div[2]')
         break
-ele.screenshot('image.png')
 
-#eleWidth = ele.size['width']
-#eleHeight = ele.size['height']
-#
-#screenshot = driver.save_screenshot('screenshot.png')
-#time.sleep(2)
-#im = Image.open("screenshot.png")
-#box = (ele.location['x'], ele.location['y'], ele.location['x']+eleWidth, ele.location['y']+eleHeight)
-#im.crop(box).save('screenshot1.png')
-#
-#print(box)
-#point = ele.location
+ele.screenshot('image.png')
